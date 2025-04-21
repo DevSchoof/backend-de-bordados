88848627e6db2985e24d79e23c4f580a4c34148f
Eduardo Schoof <eduardoschoof@gmail.com>
2025-04-21 16:43:47 -0300

    Removida a tabela usuarios do banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from app.config import Config
 from app.database import db
+from flask_migrate import Migrate  # Importa o Flask-Migrate
 from app.routes import main
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
+migrate = Migrate(app, db)  # Inicializa o Flask-Migrate
 app.register_blueprint(main)
 
 with app.app_context():
     db.create_all()
-    
